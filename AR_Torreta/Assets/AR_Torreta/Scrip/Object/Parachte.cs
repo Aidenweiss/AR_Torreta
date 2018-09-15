c11a95065e97754987e15bf519ab3c4de17033a1
abraham-tl <abraham-tl@>
2018-09-14 20:55:54 -0500

    se inicio
    se inicio

---
+++ AR_Torreta/Assets/AR_Torreta/Scrip/Object/Parachte.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parachte : MonoBehaviour {
+    float a;
+	// Use this for initialization
+	void Start () {
+        a = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.eulerAngles = new Vector3(0, 0, a * Time.deltaTime);
+       // transform.Rotate(0, 0, -a * Time.deltaTime);
+        
+        
+        if (transform.rotation.z >-20 && transform.rotation.z <= 0)
+        {
+            transform.Rotate(0, 0, -a * Time.deltaTime);
+            //transform.eulerAngles = new Vector3(0, 0, a * Time.deltaTime);
+        }
+        if(transform.rotation.z <20 && transform.rotation.z > 0)
+        {
+            transform.Rotate(0, 0,a * Time.deltaTime);
+            //transform.eulerAngles = new Vector3(0,0,-a * Time.deltaTime);
+            //transform.rotation.z = (a * Time.deltaTime);
+            //transform.Rotate(0, 0,-a * Time.deltaTime);
+        }
+		
+	}
+
+}
