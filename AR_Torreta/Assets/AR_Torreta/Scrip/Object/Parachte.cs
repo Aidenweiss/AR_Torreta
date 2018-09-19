db99078739941bb42764bd96f3daf6f27272e5e7
abraham-tl <abraham-tl>
2018-09-18 20:54:50 -0500

    Se comparte la aplicacion
    Se comparte la aplicacion

--- AR_Torreta/Assets/AR_Torreta/Scrip/Object/Parachte.cs
+++ AR_Torreta/Assets/AR_Torreta/Scrip/Object/Parachte.cs
@@ -4,30 +4,45 @@ using UnityEngine;
 
 public class Parachte : MonoBehaviour {
     float a;
-	// Use this for initialization
-	void Start () {
+    public bool sw;
+    float timer = 0;
+    float timer_destroy = 0;
+    float speed;
+    void Start () {
         a = 10;
+        sw = false;
+        timer = 1;
+        timer_destroy = 0;
+        speed = 5;
 	}
-	
-	// Update is called once per frame
 	void Update () {
-        //transform.eulerAngles = new Vector3(0, 0, a * Time.deltaTime);
-       // transform.Rotate(0, 0, -a * Time.deltaTime);
-        
-        
-        if (transform.rotation.z >-20 && transform.rotation.z <= 0)
+        timer_destroy += Time.deltaTime;
+        timer += Time.deltaTime;
+        if (timer >=2)
+        {
+            if(sw)
+            {
+                sw = false;
+            }
+            else
+            {
+                sw = true;
+            }
+            timer = 0;
+        }
+        if (sw)
+        {
+            transform.Rotate(0, 0, a * Time.deltaTime);
+        }
+        else
         {
             transform.Rotate(0, 0, -a * Time.deltaTime);
-            //transform.eulerAngles = new Vector3(0, 0, a * Time.deltaTime);
         }
-        if(transform.rotation.z <20 && transform.rotation.z > 0)
+        if(timer_destroy > 10f)
         {
-            transform.Rotate(0, 0,a * Time.deltaTime);
-            //transform.eulerAngles = new Vector3(0,0,-a * Time.deltaTime);
-            //transform.rotation.z = (a * Time.deltaTime);
-            //transform.Rotate(0, 0,-a * Time.deltaTime);
+            Destroy(this.gameObject);
         }
-		
-	}
+        transform.position -= (transform.up * speed*Time.deltaTime );
+    }
 
 }
